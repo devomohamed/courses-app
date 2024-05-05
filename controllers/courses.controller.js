843c31f9dfa95bf51d776ecc00d1cffac9877425
Mohamed Sabry <mohamed.sabry@lyveglobal.com>
2024-05-06 02:51:44 +0300

    setup mongoose and  link to database cloud

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,18 +1,25 @@
-let { courses } = require('../data/courses')
+const Course = require('../models/course.model')
 
-const getAllCourses = (req, res) => {
+const getAllCourses = async(req, res) => {
+  console.log(Course);
+  const courses = await Course.find() 
+  console.log(courses);
   res.send(courses)
 }
 
-const getSingleCourse = (req, res) => {
-  const course = courses.find(c => c.id === parseInt(req.params.id))
+const getSingleCourse = async(req, res) => {
+  try{
+    const course = await Course.findById(req.params.id)
   if (!course) {
     res.status(404).send({msg:'The course with the given ID was not found.'})
   }
   res.send(course)
+  }catch(e){
+    res.status(500).send({msg:e.message})
+  }
 }
 
-const addCourse = (req, res) => {
+const addCourse = async(req, res) => {
   try{
     const {title , price} = req.body
     if(!title){
@@ -21,48 +28,50 @@ const addCourse = (req, res) => {
     if(!price){
       return res.status(400).send({msg:'price is required'})
     }
-    const course = {
-      id: courses.length + 1,
+    const course =  new Course({
       title,
       price
-    }
-    courses.push(course)
-    res.send(course)
+    })
+    await course.save()
+    res.status(201).send(course)
   }catch(e){
     res.status(500).send(e.message)
   }
 }
 
-const updateCourse = (req,res)=>{
-  const id = +req.params.id
-
-  let course = courses.find(c => c.id === parseInt(id))
-  
+const updateCourse = async(req,res)=>{
+  let course = await Course.findById(req.params.id)
   if (!course) {
     res.status(404).send({msg:'The course with the given ID was not found.'})
   }
-  
-  const updatedCourse = req.body
-  
-  console.log(updatedCourse,"updatedCourse");
-  course = {...course,...updatedCourse}
+
+  const {title , price} = req.body
+
+  const updatedCourse = await Course.findByIdAndUpdate(req.params.id,{title,price}) 
+  await updatedCourse.save() 
+
+  course = await Course.findById(req.params.id)
   
   res.send(course)
 }
 
 
-const deleteCourse = (req,res)=>{
-  const id = +req.params.id
+const deleteCourse = async(req,res)=>{
 
-  let course = courses.find(c => c.id === parseInt(id))
+  try{
+    let course = await Course.findById(req.params.id)
   
+    console.log(course);
   if (!course) {
     res.status(404).send({msg:'The course with the given ID was not found.'})
   }
   
-  courses = courses.filter((c) => c.id != id)
-
+  const deletedCourse = await Course.findByIdAndDelete(req.params.id)
+  
   res.send({success:true})
+  }catch(e){
+    res.status(500).send(e.message)
+  }
 }
 
 
--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 var morgan = require('morgan')
+const mongoose = require('mongoose')
+
 const coursesRouter = require('./routes/courses.route')
+
+const dbName = 'madrasa'
+const url = `mongodb+srv://mohamed:KQi35wKTDi4ZjNa3@learn-mongo-db.vo1mhvv.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=learn-mongo-db`;
+mongoose.connect(url).then(()=>{console.log("mongodb server started");})
+
 
 const app = express()
 const port = 3000
 
 app.use(morgan('combined'))
+app.use(express.json())
 
 app.use('/api/courses',coursesRouter)
 
-app.use(express.json())
 
 
 
