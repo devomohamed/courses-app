bfa17761e9e7dc39c76153f31f4971e5969bd9a5
Mohamed Sabry <mohamed.sabry@lyveglobal.com>
2024-05-10 15:04:50 +0300

    code inhancement

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const coursesRouter = require('./routes/courses.route')
+const { FAIL } = require('./utils/httpStatusText')
 
 const url = process.env.MONGO_URL;
 mongoose.connect(url).then(()=>{console.log("mongodb server started");})
@@ -16,6 +17,13 @@ app.use(morgan('combined'))
 app.use(express.json())
 
 app.use('/api/courses',coursesRouter)
+app.all('*',(req,res,next)=>{
+  res.status(404).send({
+    status:FAIL,
+    data:null,
+    message:"The requested resource was not found."
+  })
+}) 
 
 
 
